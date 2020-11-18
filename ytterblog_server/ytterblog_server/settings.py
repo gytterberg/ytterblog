e344b9b6c5ab2b40742045de2bef5b6dde59f040
gytterberg <gytterberg@gmail.com>
2020-11-18 10:52:06 -0500

    buildpacks?

--- ytterblog_server/ytterblog_server/settings.py
+++ ytterblog_server/ytterblog_server/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 from pathlib import Path
 import django_heroku
 
+# arbitrary change...
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
