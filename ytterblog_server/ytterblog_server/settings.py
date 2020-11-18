8452d91d0a94f632154a63249e5303d960608fb9
gytterberg <gytterberg@gmail.com>
2020-11-18 10:40:01 -0500

    heroku?

--- ytterblog_server/ytterblog_server/settings.py
+++ ytterblog_server/ytterblog_server/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -128,3 +129,6 @@ CORS_ORIGIN_WHITELIST = [
     'https://ytterblog.herokuapp.com',
     'https://localhost:3000',
 ]
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
