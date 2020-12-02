93956bf47e9ebc5a76910b57f73b676e9f2ff9e6
gytterberg <gytterberg@gmail.com>
2020-12-02 15:44:12 -0500

    postgres?

--- ytterblog_server/settings.py
+++ ytterblog_server/settings.py
@@ -83,10 +83,21 @@ WSGI_APPLICATION = 'ytterblog_server.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'ytterblog',
+        'USER': 'ytterbloguser',
+        'PASSWORD': '6206540689',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
